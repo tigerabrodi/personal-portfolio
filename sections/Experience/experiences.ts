5be445b1dd92f1a68015a6d34a40097055bf84e9
tigerabrodi <tigerabrodi@gmail.com>
2022-07-04 09:01:12 +0200

    feat: add new experience, bobsled

--- sections/Experience/experiences.ts
+++ sections/Experience/experiences.ts
@@ -3,7 +3,7 @@ export const experiences = [
     id: 1,
     company: 'Company: Tonies - Boxine GmbH',
     role: 'Role: Frontend Developer',
-    date: 'Date: July 2020 - Present',
+    date: 'Date: July 2020 - March 2022',
     contributions: [
       'Introduced team practices that helped us move faster, such as Pair Reviews and Stop and Fix.',
       'Made the site more accessible, which is important since many of our users are elders such as grandparents.',
@@ -11,6 +11,18 @@ export const experiences = [
       'Worked on frontend with React, JavaScript/TypeScript, GraphQL (URQL Client), Styled Components, StoryBook & react-i18next, used by over 2 million users.',
       'Documented usage of performance APIs in React, which increased the code quality.',
       'Helped with organizational tasks such as moderating our weekly Dev meeting.',
+    ],
+  },
+  {
+    id: 2,
+    company: 'Company: Bobsled Inc',
+    role: 'Role: Senior Software Engineer',
+    date: 'Date: March 2022 - Present',
+    contributions: [
+      'Building the fastest data-sharing platform, a no-code data sharing experience connecting any cloud, platform, or region.',
+      'Working primarily with React, Remix, TypeScript, Cypress, Firebase and Material UI (working across the full stack).',
+      'Held an introductory internal talk about Remix.',
+      'Time to time (not everyday things) writing scripts in node or dealing with cloud tasks (such as setting up a new organization and domain in GCP).',
     ],
   },
 ]
