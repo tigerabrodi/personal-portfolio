0e6b0b37bfa565cf91281ba40518c03fd12ec740
tigerabrodi <tigerabrodi@gmail.com>
2022-07-11 08:06:16 +0200

    update home description

--- sections/Home/index.tsx
+++ sections/Home/index.tsx
@@ -17,15 +17,13 @@ export const Home = () => (
       <HomeTitle id="home">Hi, I&apos;m Tiger.</HomeTitle>
       <HomeSubtitle>I craft things for the web.</HomeSubtitle>
       <HomeDescription>
-        I’m a Software Craftsman based in Germany who is heavily into
-        Accessibility and Testing. I like building side projects,{' '}
+        I’m a Senior Software Engineer at Bobsled working across the fullstack with Remix/TypeScript/Material UI/Cloud technologies/Cypress/Firebase. I like building side projects,{' '}
         <Link href="https://tigerabrodi.hashnode.dev/" passHref>
           <DescriptionLink target="_blank" rel="noopener noreferrer">
             writing blog posts,
           </DescriptionLink>
         </Link>{' '}
-        contributing to Open Source, volunteering in communities (KCD, OSRG,
-        Testing Library, Mock Service Worker) and{' '}
+        contributing to Open Source, volunteering in communities and{' '}
         <Link
           href="https://www.goodreads.com/review/list/127384537-tiger-abrodi?ref=nav_mybooks&shelf=read"
           passHref
